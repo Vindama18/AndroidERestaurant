2bed0252a5f498f1c9ee363a281a74d403e7eee3
nicolasesposito <crazyrobot18@gmail.com>
2022-03-04 16:13:01 +0100

    second commit

--- app/src/main/java/fr/isen/esposito/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/esposito/androiderestaurant/CategoryActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +18,15 @@ val texttitle = findViewById<TextView>(R.id.textitem)
         texttitle.text=intent.getStringExtra("12345")
         setTitle(intent.getStringExtra("12345"))
 
-
-
+        val  food = CreerMesLigne()
+        var monRecycler: RecyclerView = findViewById(R.id.categoryListe)
+        monRecycler.layoutManager = LinearLayoutManager(this)
+        monRecycler.adapter = FoodAdapter(food.toTypedArray())
+        {
+            Toast.makeText(this, "Vous avez sélectionné ${it.nom}", Toast.LENGTH_SHORT).show()
+        }
 
         val back = findViewById<Button>(R.id.butback)
-
 
         // set on-click listener
         back.setOnClickListener {
@@ -28,5 +34,18 @@ val texttitle = findViewById<TextView>(R.id.textitem)
             startActivity(intent);
             Toast.makeText(this , "menu principal", Toast.LENGTH_SHORT).show()
         }
+    }
+
+
+
+    fun CreerMesLigne() :ArrayList<Food> {
+        val food = ArrayList<Food>()
+        food.add(Food(1, "Loup"))
+        food.add(Food(2, "Gorille"))
+        food.add(Food(3, "Eléphant"))
+        food.add(Food(4, "Tigre"))
+        food.add(Food(5, "Dauphin"))
+
+        return food
     }
 }
