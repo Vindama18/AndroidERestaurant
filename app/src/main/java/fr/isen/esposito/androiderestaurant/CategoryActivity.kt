651aa702c71d7c1ad966c1c8a35c8fc1a83fd4b1
nicolasesposito <crazyrobot18@gmail.com>
2022-03-04 10:59:08 +0100

    second commit

--- app/src/main/java/fr/isen/esposito/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/esposito/androiderestaurant/CategoryActivity.kt
@@ -1,11 +1,32 @@
 package fr.isen.esposito.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+
+val texttitle = findViewById<TextView>(R.id.textitem)
+        texttitle.text=intent.getStringExtra("12345")
+        setTitle(intent.getStringExtra("12345"))
+
+
+
+
+        val back = findViewById<Button>(R.id.butback)
+
+
+        // set on-click listener
+        back.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent);
+            Toast.makeText(this , "menu principal", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/fr/isen/esposito/androiderestaurant/MainActivity.kt
+++ app/src/main/java/fr/isen/esposito/androiderestaurant/MainActivity.kt
@@ -1,20 +1,46 @@
 package fr.isen.esposito.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val entre = findViewById<Button>(R.id.butentre)
+        val plat = findViewById<Button>(R.id.butplat)
+        val dessert = findViewById<Button>(R.id.butdessert)
+
+
+        // set on-click listener
+        entre.setOnClickListener {
+            startCategory(getString(R.string.butentre))
+            Toast.makeText(this , "entrées", Toast.LENGTH_SHORT).show()
+        }
+
+        // set on-click listener
+        plat.setOnClickListener {
+            startCategory(getString(R.string.butplat))
+            Toast.makeText(this, "plats", Toast.LENGTH_SHORT).show()
+        }
+
+        // set on-click listener
+        dessert.setOnClickListener {
+            startCategory(getString(R.string.butdessert))
+            Toast.makeText(this, "desserts", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
-  /*  butdessert.setOnClickListener(object: View.OnClickListener {
-        override fun onClick(view: View): Unit {
-            //Handler code here.
-            val intent = Intent(context, dessert::class.java);
-            startActivity(intent);
-        }
-    }*/
+    private fun startCategory(category : String) {
+        val intent =
+            Intent(this, CategoryActivity::class.java).apply { putExtra("12345", category) }
+        startActivity(intent);
+    }
 }
 
